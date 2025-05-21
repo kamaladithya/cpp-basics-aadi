235ccaea258b420cc44ae7c5ef0bd77c3ac2cb80
kamaladithya <75839104+kamaladithya@users.noreply.github.com>
2025-05-21 14:15:18 +0530

    Add files via upload

---
+++ day2.cpp
@@ -0,0 +1,40 @@
+//Qn1
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int roll = 27;
+// cout<<"Roll Number: "<<roll<<endl;
+// float percentage = 92.5;
+// cout<<"Percentage: "<<percentage<<endl;
+// char grade = 'A';
+// cout<<"Grade: "<<grade<<endl;
+// bool passed = true;
+// cout<<"Passed: "<<passed<<endl;
+// double cgpa = 9.43;
+// cout<<"CGPA: "<<cgpa<<endl;
+
+// return 0;
+// }
+
+//Qn2
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     const float PI = 3.14159;
+//     float radius = 5.5;
+//     float area;
+//     area = PI * radius * radius;
+//     cout<<"Radius: "<<radius<<endl;
+//     cout<<"Area of Circle: "<<area<<endl;
+//     return 0;
+// }
+
+//Qn3
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hi Aadi,\n";
+    cout<<"Welcome to C++!\n";
+    cout<<"Let's rock \"CodeWithHarry\"style.\\nNext line begins here.";
+    return 0;
+}
